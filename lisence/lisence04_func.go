c94b564009efe161068adc5c54bd33ef17feb064
rogetsun <songyw163@163.com>
2016-10-24 15:20:17 +0800

    commit

--- lisence/lisence04_func.go
+++ lisence/lisence04_func.go
@@ -13,8 +13,9 @@ func (i *Int)increment() Int {
 func main() {
 	var x Int = 11
 	fmt.Println(x.increment())
-	x.increment()
+	x.increment()//对象直接调用方式
 	x.increment()
 	fmt.Println(x)
+	(*Int).increment(&x)//第二种调用方法，像js的call
 	fmt.Println(x + 10)
 }
