9bc7a52eb6b41846975d2f1c7948c6d8ca063e21
rogetsun <songyw163@163.com>
2017-09-08 19:49:15 +0800

    fmt

--- lisence/lisence08_int2string.go
+++ lisence/lisence08_int2string.go
@@ -5,10 +5,20 @@ import (
 	"strconv"
 )
 
+const (
+	UB int64 = 1 << (10 * iota)
+	KB
+	GB
+	TB
+)
+
 func main() {
+
 	a := 65
 	fmt.Println(string(a))
 	fmt.Println(strconv.Itoa(a))
 	fmt.Println(strconv.Atoi("65"))
 	fmt.Println(int('a'))
+	fmt.Printf("B=%dbyte, KB=%dbyte, GB=%dbyte, TB=%dbyte", UB, KB, GB, TB)
+
 }
